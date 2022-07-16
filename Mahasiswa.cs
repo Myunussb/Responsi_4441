505dc3b2270c405d0f7a2b2e39902d7ba3fd1f80
Myunussb <muhammadyunus16@students.amikom.ac.id>
2022-07-16 10:21:39 +0700

    Responsi

---
+++ Mahasiswa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    public class Mahasiswa
+    {
+        // PERINTAH: lengkapi property class mahasiswa, sesuai petunjuk soal
+        public string NIM { get; set; }
+        public string NAMA { get; set; }
+        public string GENDER { get; set; }
+        public string IPK { get; set; }
+    }
+}
